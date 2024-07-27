976f7a9a41e53791cc5e7013b687abe87286ce5c
AlamMartins <alammartins@haf.com.br>
2024-07-27 01:04:01 -0300

    update fix

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
-require("dotenv").config()
+// require("dotenv").config()
+
+// const express = require('express')
+// const path = require("path")
+// const cors = require("cors")
+
+// const app = express()
+// const port = process.env.PORT || 4000
+
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `)
+// })
+
+// // Configurando JSON e form data response
+// // app.use(express.json())
+// // app.use(express.urlencoded({extended:false}))
+
+// // //Resolvendo CORS
+// // app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+// // //Upload directory
+// // app.use("/uploads", express.static(path.join(__dirname,"/uploads")))
+
+// // //DB connection - Conectando o banco de dados
+// // require("./config/db.js")
+
+// //routes
+// // const router = require("./routes/Router.js")
+
+// // app.use(router)
+
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳')
+// })
+
+// // app.get('/about', (req, res) => {
+// //   res.send('This is my about route..... ')
+// // })
+
+// // Export the Express API
+// module.exports = app
+
 
 const express = require('express')
-const path = require("path")
-const cors = require("cors")
 
 const app = express()
-const port = process.env.PORT || 4000
-
+const PORT = 4000
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-// Configurando JSON e form data response
-// app.use(express.json())
-// app.use(express.urlencoded({extended:false}))
-
-// //Resolvendo CORS
-// app.use(cors({credentials: true, origin: "http://localhost:3000"}));
-
-// //Upload directory
-// app.use("/uploads", express.static(path.join(__dirname,"/uploads")))
-
-// //DB connection - Conectando o banco de dados
-// require("./config/db.js")
-
-//routes
-// const router = require("./routes/Router.js")
-
-// app.use(router)
-
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
-// app.get('/about', (req, res) => {
-//   res.send('This is my about route..... ')
-// })
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
 // Export the Express API
 module.exports = app
